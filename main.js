bc8e503b86f3894103cb827e4d90e7419c7da02c
AtayevZarif <atayev1234567890@gmail.com>
2024-10-15 17:21:46 +0500

    indev-14

--- main.js
+++ main.js
@@ -1,11 +1,39 @@
 let telegram = window.Telegram.WebApp;
-let name = document.getElementById("name")
-let phone = document.getElementById("phone")
-let email = document.getElementById("email")
+let name = document.getElementById("name").value
+let phone = document.getElementById("phone").value
+let email = document.getElementById("email").value
+let error = document.getElementById("error")
 let buyBtn = document.querySelector("submit")
 
-name.value = telegram.initDataUnsafe.first_name + " " + telegram.initDataUnsafe.last_name
+telegram.expand()
+
+name = telegram.initDataUnsafe.user.first_name + " " + telegram.initDataUnsafe.user.last_name
+
 
 buyBtn.addEventListener("click", () => {
-     
+    error.innerText = ""
+
+    if (name.length < 5){
+        error.innerText = "Ismda xato bor"
+        return
+    }
+
+    if (phone.length < 5){
+        error.innerText = "Telefon raqamda xato bor"
+        return
+    }
+
+    if (email.length < 5){
+        error.innerText = "Emailda xato bor"
+        return
+    }
+
+    let data = {
+        name: name,
+        phone: phone,  
+        email: email,
+    }
+    telegram.sendData("")
+
+    telegram.close()
 })
